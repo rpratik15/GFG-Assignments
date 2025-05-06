0abf8998147c8011a6b324f472b406188ca591a4
Pratik Raval <rpratik15@gmail.com>
2025-05-06 17:37:16 +0530

    assignment9 done

--- assignment9/card-app/src/App.jsx
+++ assignment9/card-app/src/App.jsx
@@ -4,12 +4,38 @@ import Card from './Components/Card'
 import './App.css'
 
 function App() {
-
+const [cards, setCards] = useState([
+  {
+    id: 1,
+    image: 'https://cdn2.thecatapi.com/images/IFXsxmXLm.jpg',
+    title: 'Cat',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, illum.'
+  },
+  {
+    id: 2,
+    image: 'https://plus.unsplash.com/premium_photo-1666878155781-f86514e5808b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Dog',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, illum.'
+  },
+  {
+    id: 3,
+    image: 'https://images.unsplash.com/photo-1480044965905-02098d419e96?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Bird',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, illum.'
+  }
+])
 
   return (
-    <>
-      <Card/> 
-    </>
+    <div className='App'>
+    {
+      cards.map((card)=>{
+        return(
+          <Card key={card.id} image={card.image} title={card.title} description={card.description}/>
+        )
+      })
+    }
+      
+    </div>
   )
 }
 
--- assignment9/card-app/src/Components/Card.jsx
+++ assignment9/card-app/src/Components/Card.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import '../Style/Card.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons' 
 
 function Card({image, title, description}) {
   return (
     <div className='Card'>
-        <img src="https://cdn2.thecatapi.com/images/LSaDk6OjY.jpg" alt={title} className='Card-image' />
-        <h2 className='Card-title'>Cat</h2>
-        <p className='Card-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, illum.</p>
-        <div className='Card- buttons'>
-            <div className="save">
+        <img src={image} alt="title" className='Card-image' />
+        <h2 className='Card-title'>{title}</h2>
+        <p className='Card-description'>{description}.</p>
+        <div className='Card-buttons'>
+            <div className="grp1">
             <i class="fa-regular fa-bookmark fa-lg"></i>
             </div>
+            <div className="grp2">
             <i class="fa-regular fa-thumbs-up fa-lg"></i>
             <i class="fa-regular fa-thumbs-down fa-lg"></i>
             <i class="fa-regular fa-paper-plane fa-lg"></i>
+            </div>
         </div>
     </div>
   )
