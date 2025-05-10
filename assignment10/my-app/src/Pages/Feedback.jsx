5b715f91e896fc7c5a15585338e515f1a8d1b8c8
Pratik Raval <rpratik15@gmail.com>
2025-05-10 17:19:42 +0530

    assignment10 final

--- assignment10/my-app/src/Pages/Feedback.jsx
+++ assignment10/my-app/src/Pages/Feedback.jsx
@@ -1,20 +1,28 @@
 import React, { useState,useEffect } from 'react'
 
 function Feedback() {
+  const initialValue={
+    name:"",  
+    age:"",
+    city:""}
   const [user,setUser]=useState([])
-    const [data,setData]=useState([{
-        name:"",
-        age:"",
-        city:""
+    const [data,setData]=useState([{initialValue
     }])
     const SaveData=(e)=>{
     e.preventDefault();
     setUser([...user,data])
-    alert(`Name :${data.name} Age :${data.age} City :${data.city}`)
-    
+   
+    setData(initialValue)
 
     }
     
+    const handleChange=(e)=>{
+        const {name,value}=e.target;
+        setData((prev)=>({
+            ...prev,
+            [name]:value
+        }))
+    }
     useEffect(()=>{
         console.log(user)
     },[user])
@@ -25,11 +33,26 @@ function Feedback() {
     <div className='feedback'>Feedback Form 
         <div className="myForm">
             <form action="" onSubmit={SaveData}>
-                <input type='text' placeholder='Enter your name' value={data.name} onChange={(e)=>setData({...data,name:e.target.value})}/>
-                <input type='text' placeholder='Enter your age' value={data.age} onChange={(e)=>setData({...data,age:e.target.value})}/>
-                <input type='text' placeholder='Enter your city' value={data.city} onChange={(e)=>setData({...data,city:e.target.value})}/>
+                <input type='text' placeholder='Enter your name' name='name' value={data.name} onChange={handleChange}/>
+                <input type='text' placeholder='Enter your age' name='age' value={data.age} onChange={handleChange}/>
+                <input type='text' placeholder='Enter your city' name='city' value={data.city} onChange={handleChange}/>
                 <button type='submit'>Submit</button>
             </form>
+        </div>
+        <div className="card">
+          {
+            user && user.map((item,index)=>{
+              return(
+                <div className='cardItem' key={index}>
+                  <h3>Name : {item.name}</h3>
+                  <h3>Age : {item.age}</h3>
+                  <h3>City : {item.city}</h3>
+                  </div>
+                  
+              )}
+            )
+             
+      }
         </div>
     </div>
   )
